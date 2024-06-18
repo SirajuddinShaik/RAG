0321c026dbe9860272d49b887da6543c70ef374e
siraj9949 <shaiksirajuddin9949@gmail.com>
2024-06-18 19:19:51 +0530

    chainlit

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from io import BytesIO
-from langchain.schema.runnable import Runnable
-from langchain.schema.runnable.config import RunnableConfig
 import fitz
 from pinecone import Pinecone
 
-from chainlit.input_widget import Select,TextInput
+from chainlit.input_widget import Select, TextInput
 import chainlit as cl
 from tqdm.auto import tqdm
 
@@ -15,20 +13,32 @@ from RAG.pipeline.stage_02_search_answer import SearchAnswerPipeline
 model_select = Select(
     id="Model",
     label="select - Model",
-    values=["openai-community/gpt2", "google/gemma-2b-it", "openai-community/gpt2-xl", "microsoft/Phi-3-vision-128k-instruct"],
+    values=[
+        "openai-community/gpt2",
+        "google/gemma-2b-it",
+        "openai-community/gpt2-xl",
+        "microsoft/Phi-3-vision-128k-instruct",
+    ],
     initial_index=0,
 )
 
 query_type = Select(
     id="Query",
     label="Select Task - Type",
-    values=["detailed_prompt","short_prompt", "summary_prompt", "explanation_prompt", "opinion_prompt", "instruction_prompt"],
+    values=[
+        "detailed_prompt",
+        "short_prompt",
+        "summary_prompt",
+        "explanation_prompt",
+        "opinion_prompt",
+        "instruction_prompt",
+    ],
     initial_index=0,
 )
 pdf_select = Select(
     id="Pdf",
     label="Select Pdf - Slot",
-    values=["slot-1","slot-2", "slot-3", "slot-4", "slot-5"],
+    values=["slot-1", "slot-2", "slot-3", "slot-4", "slot-5"],
     initial_index=0,
 )
 hf_api = TextInput(
@@ -37,15 +47,13 @@ hf_api = TextInput(
     placeholder="hf-...",
 )
 pc_api = TextInput(
-    id="pc_key",
-    label="Enter your Pinecone API key",
-    placeholder="sk-..."
+    id="pc_key", label="Enter your Pinecone API key", placeholder="sk-..."
 )
+
 
 @cl.on_message
 async def on_message(message: cl.Message):
     print(message.content)  # type: Runnable
-
 
     await cl.Message(content="hlo how are you").send()
 
@@ -59,7 +67,9 @@ async def start():
     cl.user_session.set("hf", "")
     cl.user_session.set("model", "")
     cl.user_session.set("task", "")
-    cl.user_session.set("paths", {"slot-1": "", "slot-2": "", "slot-3": "", "slot-4": "", "slot-5": ""})
+    cl.user_session.set(
+        "paths", {"slot-1": "", "slot-2": "", "slot-3": "", "slot-4": "", "slot-5": ""}
+    )
     cl.user_session.set("slot", "slot-1")
     cl.user_session.set("err", "Setup Api Keys")
     cl.user_session.set("api_status", False)
@@ -75,11 +85,11 @@ async def start():
             pdf_select,
         ]
     ).send()
-    
+
     model1 = settings["Model"]
     model2 = settings["Query"]
     api_key = settings["hf_key"]
-    
+
     # You can now use the api_key variable in your application
     print(f"API Key: {api_key}")
     print(f"Selected Model 1: {model1}")
@@ -101,7 +111,14 @@ async def on_message(msg: cl.Message):
         return
     else:
         obj = cl.user_session.get("searcher")
-        ans = obj.chainlit_prompt(msg.content, cl.user_session.get("task"), cl.user_session.get("pc"), cl.user_session.get("hf"), cl.user_session.get("slot"), cl.user_session.get("model"))
+        ans = obj.chainlit_prompt(
+            msg.content,
+            cl.user_session.get("task"),
+            cl.user_session.get("pc"),
+            cl.user_session.get("hf"),
+            cl.user_session.get("slot"),
+            cl.user_session.get("model"),
+        )
         if ans:
             msg = cl.Message(content=ans[0]["generated_text"])
             await msg.send()
@@ -111,13 +128,9 @@ async def on_message(msg: cl.Message):
         return
 
 
-
-
-
-
 async def load_pdf_to_pinecone():
     index_name = cl.user_session.get("slot")
-    
+
     res = await cl.AskActionMessage(
         content="Ready to Upload!",
         actions=[
@@ -125,30 +138,30 @@ async def load_pdf_to_pinecone():
             cl.Action(name="cancel", value="cancel", label="❌ Cancel"),
         ],
     ).send()
-    
+
     if res and res.get("value") == "continue":
         files = await cl.AskFileMessage(
-                content=f"Please upload a PDF file to load in {index_name}, `Please Wait!`",
-                accept=["application/pdf"],
-                max_size_mb=20,
-                timeout=180,
-            ).send()
-        
+            content=f"Please upload a PDF file to load in {index_name}, `Please Wait!`",
+            accept=["application/pdf"],
+            max_size_mb=20,
+            timeout=180,
+        ).send()
+
         file = files[0]
         dict = cl.user_session.get("paths")
         dict[index_name] = file.path
         obj1 = cl.user_session.get("searcher")
-        cl.user_session.set("paths",dict)
+        cl.user_session.set("paths", dict)
         obj = DataIngestionPipeLine()
         obj.load_to_pincone(cl.user_session.get("pc"), file.path, index_name)
         obj1.query_answer.setup_pd(dict)
         msg = cl.Message(content=f"Uploaded to {index_name}! `Start messaging...`")
-        await msg.send() 
+        await msg.send()
 
 
 async def load_pdf():
     index_name = cl.user_session.get("slot")
-    
+
     res = await cl.AskActionMessage(
         content="Ready to Upload!",
         actions=[
@@ -156,25 +169,26 @@ async def load_pdf():
             cl.Action(name="cancel", value="cancel", label="❌ Cancel"),
         ],
     ).send()
-    
+
     if res and res.get("value") == "continue":
         files = await cl.AskFileMessage(
-                content=f"Please upload a PDF file to load in {index_name}, `Please Wait!`",
-                accept=["application/pdf"],
-                max_size_mb=20,
-                timeout=180,
-            ).send()
-        
+            content=f"Please upload a PDF file to load in {index_name}, `Please Wait!`",
+            accept=["application/pdf"],
+            max_size_mb=20,
+            timeout=180,
+        ).send()
+
         file = files[0]
         dict = cl.user_session.get("paths")
         dict[index_name] = file.path
-        cl.user_session.set("paths",dict)
+        cl.user_session.set("paths", dict)
         obj = DataIngestionPipeLine()
         obj1 = cl.user_session.get("searcher")
         obj.store_tokens(cl.user_session.get("pc"), file.path, index_name)
         obj1.query_answer.setup_pd(dict)
         msg = cl.Message(content=f"Uploaded to {index_name}! `Start messaging...`")
         await msg.send()
+
 
 @cl.on_settings_update
 async def verify_keys(settings):
@@ -185,19 +199,22 @@ async def verify_keys(settings):
             cl.user_session.set("searcher", SearchAnswerPipeline(settings["pc_key"]))
             cl.user_session.set("pc", settings["pc_key"])
         except:
-            message ="Invalid Pinecone Key"
+            message = "Invalid Pinecone Key"
             cl.user_session.set("err", message)
             return
         import requests
+
         API_TOKEN = settings["hf_key"]
         API_URL = "https://api-inference.huggingface.co/models/gpt2"
         headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
         def query(payload):
             response = requests.post(API_URL, headers=headers, json=payload)
             return response
+
         response = query("Can you please let us know more details about your ")
         if response.status_code == 400:
-            message ="Invalid Hugging Face Token"
+            message = "Invalid Hugging Face Token"
             cl.user_session.set("err", message)
             return
         cl.user_session.set("hf", settings["hf_key"])
@@ -205,4 +222,3 @@ async def verify_keys(settings):
     cl.user_session.set("model", settings["Model"])
     cl.user_session.set("slot", settings["Pdf"])
     cl.user_session.set("task", settings["Query"])
-        
